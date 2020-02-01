c72b514eece52d5b69e5a7a45ef9c306440d026f
KittyChen <kimikamis@gmail.com>
2020-02-01 22:52:59 +0800

    modify format

--- src/components/DealDetails.vue
+++ src/components/DealDetails.vue
@@ -1,124 +1,118 @@
 <template>
-<v-container class="px-0">
-	<v-layout row class="mx-auto">
-<v-flex xs12 sm10 md6 lg4>
-  <v-card tile
-	class="mb-6"
-	xs12 md6 lg4
-	v-for="deal in details"
-	:key="deal.id"
-	>		
-    <v-img
-      src="../assets/cross.jpg"
-      height="200px"
-    ></v-img>
-	<v-layout row class="mx-3 px-1 mb-0 pb-0">
-    <v-card-title class="mb-0 pb-0">
-      {{deal.description}}
-    </v-card-title>
-</v-layout>
-<v-layout row class="ml-3 mr-3 mt-0 px-1 pt-0">
-    <v-card-subtitle class="font-weight-bold mt-1 pt-1">
-		{{deal.end_time}}
-    </v-card-subtitle>
-    <v-spacer></v-spacer>
-	<v-btn class="mt-0 pt-0" text>
-        <v-icon small>mdi-map-marker</v-icon>200 m
-     </v-btn>
-     <v-card-text class="py-0">
-			{{deal.description}}
-		</v-card-text>
-     </v-layout>
-    <v-card-actions class="pt-0 pb-0 mb-0 mt-0">
+  <v-container class="px-0">
+    <v-layout row class="mx-auto">
+      <v-flex xs12 sm10 md6 lg4>
+        <v-card tile
+                class="mb-6"
+                xs12 md6 lg4
+                v-for="deal in details"
+                :key="deal.id">		
+          <v-img src="../assets/cross.jpg" height="200px"></v-img>
+          <v-layout row class="mx-3 px-1 mb-0 pb-0">
+            <v-card-title class="mb-0 pb-0">
+              {{deal.description}}
+            </v-card-title>
+          </v-layout>
+          <v-layout row class="ml-3 mr-3 mt-0 px-1 pt-0">
+            <v-card-subtitle class="font-weight-bold mt-1 pt-1">
+              {{deal.end_time}}
+            </v-card-subtitle>
+            <v-spacer></v-spacer>
+            <v-btn class="mt-0 pt-0" text>
+                <v-icon small>mdi-map-marker</v-icon>200 m
+            </v-btn>
+            <v-card-text class="py-0">
+              {{deal.description}}
+            </v-card-text>
+          </v-layout>
+          
+          <v-card-actions class="pt-0 pb-0 mb-0 mt-0">
+            <v-spacer></v-spacer>
 
-      <v-spacer></v-spacer>
+            <v-btn
+              class="pt-0 pb-0 mt-n1 mb-1"
+              text
+              large
+              block
+              right
+              @click.native="deal.show = !deal.show"
+            >
+              <v-icon>{{ deal.show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+            </v-btn>
+          </v-card-actions>
 
-      <v-btn
-		class="pt-0 pb-0 mt-n1 mb-1"
-        text
-        large
-        block
-        right
-        @click.native="deal.show = !deal.show"
-      >
-       <v-icon>{{ deal.show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-      </v-btn>
-    </v-card-actions>
+          <v-expand-transition>
+            <div v-show="deal.show">
+              <v-layout row class="mx-3 px-1 mb-0 pb-0">
+                  <v-card-text class="mt-n1.5 pt-0">
+                    <v-span class="font-weight-bold">Disclaimer</v-span>
+                    <br>
+                    {{deal.disclaimer}}
+                  </v-card-text>
+                  <v-layout row class="d-flex justify-space-around">
+                    <v-btn width="35vw" dark color="#DFA937" tile class="buttons" depressed @click.stop="deal.dialog = true">
+                      get coupon
+                    </v-btn>
+                    <v-dialog
+                      v-model="deal.dialog"
+                      max-width="290">
+                      <v-card>
+                        <v-layout row class="mx-auto">
+                          <v-card-title class="headline">Success!</v-card-title>
+                          <v-spacer></v-spacer>
+                          <v-btn icon @click="deal.dialog = false">
+                            <v-icon>mdi-close</v-icon>
+                          </v-btn>
+                        </v-layout>
+                        <v-card-text>
+                          You have successfully claimed this deal! Make sure you read the disclaimer and note the time when the coupon can be claimed. 
+                        </v-card-text>
 
-    <v-expand-transition>
-      <div v-show="deal.show">
-		<v-layout row class="mx-3 px-1 mb-0 pb-0">
-        <v-card-text class="mt-n1.5 pt-0"><v-span class="font-weight-bold">Disclaimer</v-span><br>{{deal.disclaimer}}
-        </v-card-text>
-        <v-layout row class="d-flex justify-space-around">
-			<v-btn width="35vw" dark color="#DFA937" tile class="buttons" depressed @click.stop="deal.dialog = true">
-				get coupon
-			</v-btn>
-			<v-dialog
-      v-model="deal.dialog"
-      max-width="290"
-    >
-      <v-card>
-			<v-layout row class="mx-auto">
-        <v-card-title class="headline">Success!</v-card-title>
-		<v-spacer></v-spacer>
-        <v-btn icon @click="deal.dialog = false">
-            <v-icon>mdi-close</v-icon>
-          </v-btn>
-        </v-layout>
-
-        <v-card-text>
-          You have successfully claimed this deal! Make sure you read the disclaimer and note the time when the coupon can be claimed. 
-        </v-card-text>
-
-        <v-card-actions class="d-flex justify-center pb-3">
-
-          <v-btn
-            width="80%" dark color="#DFA937" tile class="buttons" depressed to="/profile"
-          >
-            see in profile
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-			<v-btn width="35vw" tile class="buttonst" depressed>
-				share
-			</v-btn>
-		</v-layout>
-		</v-layout>
-		<v-divider class="my-5"></v-divider>
-	
-    <v-layout row class="mx-5">
-		<v-list-item>
-			<v-list-item-avatar size="54" color="grey">
-				<img
-        src="../assets/avatar.png"
-        alt="John"
-      >
-			</v-list-item-avatar>
-			<v-list-item-content>
-				<v-list-item-title class="headline">{{deal.vendor_name}}</v-list-item-title>
-			</v-list-item-content>
-		</v-list-item>
-		<v-btn text class="pb-0 mb-n3">
-		<v-span class="font-weight-bold body">{{deal.vendor_address}}</v-span>
-		
-		<v-icon class="ml-12" right>mdi-map</v-icon>
-	</v-btn>
-		<v-card-text class="mb-3">
-			{{deal.description}}
-		</v-card-text>
+                        <v-card-actions class="d-flex justify-center pb-3">
+                          <v-btn
+                            width="80%" dark color="#DFA937" tile class="buttons" depressed to="/profile"
+                          >
+                            see in profile
+                          </v-btn>
+                        </v-card-actions>
+                      </v-card>
+                    </v-dialog>
+                    <v-btn width="35vw" tile class="buttonst" depressed>
+                      share
+                    </v-btn>
+                  </v-layout>
+              </v-layout>
+              <v-divider class="my-5"></v-divider>
+        
+              <v-layout row class="mx-5">
+                <v-list-item>
+                  <v-list-item-avatar size="54" color="grey">
+                    <img src="../assets/avatar.png"
+                        alt="John">
+                  </v-list-item-avatar>
+                  <v-list-item-content>
+                    <v-list-item-title class="headline">{{deal.vendor_name}}</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+                <v-btn text class="pb-0 mb-n3">
+                  <v-span class="font-weight-bold body">{{deal.vendor_address}}</v-span>
+                  
+                  <v-icon class="ml-12" right>mdi-map</v-icon>
+                </v-btn>
+                <v-card-text class="mb-3">
+                  {{deal.description}}
+                </v-card-text>
+              </v-layout>
+              <v-img
+                src="../assets/storefront.jpg"
+                height="300px"
+              ></v-img>
+            </div>
+          </v-expand-transition>
+        </v-card>
+      </v-flex>
     </v-layout>
-    <v-img
-      src="../assets/storefront.jpg"
-      height="300px"
-    ></v-img>
-      </div>
-    </v-expand-transition>
-  </v-card>
-</v-flex>
-</v-layout>
-</v-container>
+  </v-container>
 </template>
 
 <script>
@@ -154,9 +148,10 @@ export default {
 		return {
 			details: []
 		}
-    },
+  },
 }
 </script>
+
 <style scoped>
 .buttons {
 	border-radius: 5px;
--- src/components/VendorCard.vue
+++ src/components/VendorCard.vue
@@ -1,242 +1,214 @@
 <template>
 	<div class="vendorcard">
-		
-	<v-layout row class="mx-auto">
-<!-- <v-flex xs12 sm10 md6 lg4> -->
-  <v-card tile
-	class="mb-6"
-	width="85vw"
-	xs12 md6 lg4
-	v-for="(deal, index) in details"
-	:key="deal.id"
-	>		
-    <v-img
-      src="../assets/cross.jpg"
-      height="200px"
-    ></v-img>
-	<v-layout row class="mx-3 px-1 mb-0 pb-0">
-    <v-card-title class="mb-0 pb-0">
-      {{deal.title}}
-    </v-card-title>
-</v-layout>
-<v-layout row class="ml-3 mr-3 mt-0 px-1 pt-0">
-    <v-card-subtitle class="font-weight-bold mt-1 pt-1">
-		{{deal.time1}}
-    </v-card-subtitle>
-    <v-spacer></v-spacer>
-     <v-card-text class="py-0">
-			{{deal.description}}
-		</v-card-text>
-		<v-card-text class="mt-2 pt-0"><v-span class="font-weight-bold">Disclaimer</v-span><br>{{deal.disclaimer}}
-        </v-card-text>
-        <v-row justify="space-around" class="mx-2 align-center">
-			<v-switch inset @change="deal.dialog3 = true" color="success"></v-switch>
-			<v-dialog v-model="deal.dialog3"
-      max-width="290">
-      <v-card>
-			<v-layout row class="mx-auto">
-        <v-card-title class="headline">Activate deal</v-card-title>
-		<v-spacer></v-spacer>
-        <v-btn icon @click="deal.dialog3 = false">
-            <v-icon>mdi-close</v-icon>
-          </v-btn>
-        </v-layout>
-				<v-form
-    ref="form"
-    v-model="valid"
-    lazy-validation
-    class="mx-5"
-  >
-			<v-checkbox
-	v-model="allday"
-	color="green"
-	>
-	<template  v-slot:label>
-		<div>
-			All day deal
-		</div>
-	</template>
-	</v-checkbox>
-	<v-text-field
-      v-model="start"
-      label="Start-time"
-      required
-      color="#DFA937"
-    ></v-text-field>
-    <v-text-field
-      v-model="end"
-      label="End-time"
-      required
-      color="#DFA937"
-    ></v-text-field>
-</v-form>
-<v-card-actions class="d-flex justify-space-around pb-3">
-
-          <v-btn
-            width="90%" dark color="#DFA937" tile class="buttons" depressed @click="deal.dialog3 = false, snackbar = true">
-            save & post
-          </v-btn>
-          <v-snackbar
-			v-model="snackbar" color="success">
-			You have successfully activated this deal. It is now live and is viseble for customers!
-			<v-btn
-			vertical
-			text
-			dark
-			@click="deal.snackbar = false">close</v-btn>
-			</v-snackbar>
-           </v-card-actions>
-      </v-card>
-    
-
-</v-dialog>
-
-			<v-btn fab text @click="deal.dialog2 = true">
-				<v-icon right>mdi-pencil</v-icon>
-			</v-btn>
-			<v-dialog
-      v-model="deal.dialog2"
-      max-width="290">
-      <!-- <v-card> -->
-			
-		<v-card>
-			<v-layout row class="mx-auto">
-        <v-card-title class="headline">Edit deal</v-card-title>
-		<v-spacer></v-spacer>
-        <v-btn icon @click="deal.dialog2 = false">
-            <v-icon>mdi-close</v-icon>
-          </v-btn>
-        </v-layout>
-				<v-form
-    ref="form"
-    v-model="valid"
-    lazy-validation
-    class="mx-5"
-  >
-
-    <v-text-field
-      v-model="title"
-      label="Title"
-      required
-      color="#DFA937"
-    ></v-text-field>
-    <v-textarea
-        v-model="description"
-        color="#DFA937"
-        >
-        <template v-slot:label>
-            <div>
-              Description 
-            </div>
-        </template>
-    </v-textarea>
-	<v-checkbox
-	v-model="allday"
-	color="green"
-	>
-	<template  v-slot:label>
-		<div>
-			All day deal
-		</div>
-	</template>
-	</v-checkbox>
-	<v-text-field
-      v-model="start"
-      label="Start-time"
-      required
-      color="#DFA937"
-    ></v-text-field>
-    <v-text-field
-      v-model="end"
-      label="End-time"
-      required
-      color="#DFA937"
-    ></v-text-field>
-    <!-- <v-text-field
-      v-model="photo"
-      label="Deal Photo"
-      color="#DFA937"
-      append-icon="mdi-camera-plus"
-    ></v-text-field> -->
-    <v-file-input
-		label="Deal Photo"
-		prepend-icon="mdi-camera"
-	></v-file-input>
-	
-		
-  </v-form>
-<!-- </v-layout> -->
-<!-- </v-card> -->
-
-        <v-card-actions class="d-flex justify-space-around pb-3">
-
-          <v-btn
-            width="50%" dark color="#DFA937" tile class="buttons" depressed @click="deal.dialog2 = false">
-            save & post
-          </v-btn>
-          <v-btn
-            width="40%" dark color="#DFA937" tile class="buttonst" depressed @click="deal.dialog2 = false">
-            save
-          </v-btn>
-          <v-snackbar
-			v-model="snackbar">
-			You have successfully edited this deal. Toggle the switch to make the deal live and visable for customers!
-			<v-btn
-			color="success"
-			vertical
-			text
-			dark
-			@click="deal.snackbar = false">close</v-btn>
-			</v-snackbar>
-           </v-card-actions>
-      </v-card>
-    </v-dialog>
-
-			<v-btn fab text @click="deal.dialog = true">
-				<v-icon right>mdi-delete</v-icon>
-			</v-btn>
-			<v-dialog
-      v-model="deal.dialog"
-      max-width="290">
-      <v-card>
-			<v-layout row class="mx-auto">
-        <v-card-title class="headline">Delete Deal</v-card-title>
-		<v-spacer></v-spacer>
-        <v-btn icon @click="deal.dialog = false">
-            <v-icon>mdi-close</v-icon>
-          </v-btn>
+    <v-layout row class="mx-auto">
+      <v-card tile
+              class="mb-6"
+              width="85vw"
+              xs12 md6 lg4
+              v-for="(deal, index) in details"
+              :key="deal.id">		
+        <v-img src="../assets/cross.jpg"
+              height="200px">
+        </v-img>
+        <v-layout row class="mx-3 px-1 mb-0 pb-0">
+          <v-card-title class="mb-0 pb-0">
+            {{deal.title}}
+          </v-card-title>
         </v-layout>
 
-        <v-card-text>
-          Are you sure you want to cancel this deal? This action is permanent.
-        </v-card-text>
+        <v-layout row class="ml-3 mr-3 mt-0 px-1 pt-0">
+          <v-card-subtitle class="font-weight-bold mt-1 pt-1">
+            {{deal.time1}}
+          </v-card-subtitle>
+          <v-spacer></v-spacer>
+          <v-card-text class="py-0">
+            {{deal.description}}
+          </v-card-text>
+          <v-card-text class="mt-2 pt-0">
+            <v-span class="font-weight-bold">
+              Disclaimer
+            </v-span>
+            <br>
+            {{deal.disclaimer}}
+          </v-card-text>
+          <v-row justify="space-around" class="mx-2 align-center">
+            <v-switch inset @change="deal.dialog3 = true" color="success"></v-switch>
+            <v-dialog v-model="deal.dialog3"
+                      max-width="290">
+              <v-card>
+                <v-layout row class="mx-auto">
+                  <v-card-title class="headline">Activate deal</v-card-title>
+                  <v-spacer></v-spacer>
+                    <v-btn icon @click="deal.dialog3 = false">
+                      <v-icon>mdi-close</v-icon>
+                    </v-btn>
+                </v-layout>
 
-        <v-card-actions class="d-flex justify-center pb-3">
+                <v-form ref="form"
+                        v-model="valid"
+                        lazy-validation
+                        class="mx-5">
+                  <v-checkbox v-model="allday"
+                              color="green">
+                    <template v-slot:label>
+                      <div>
+                        All day deal
+                      </div>
+                    </template>
+                  </v-checkbox>
+                  <v-text-field v-model="start"
+                              label="Start-time"
+                              required
+                              color="#DFA937">
+                  </v-text-field>
+                  <v-text-field v-model="end"
+                                label="End-time"
+                                required
+                                color="#DFA937">
+                  </v-text-field>
+                </v-form>
 
-          <v-btn
-            width="80%" dark color="#DFA937" tile class="buttons" depressed @click="remove(index)"
-          >
-            yes, i am sure
-          </v-btn>
+                <v-card-actions class="d-flex justify-space-around pb-3">
+                  <v-btn
+                    width="90%" dark color="#DFA937" tile class="buttons" depressed @click="deal.dialog3 = false, snackbar = true">
+                    save & post
+                  </v-btn>
+                  <v-snackbar v-model="snackbar" color="success">
+                    You have successfully activated this deal. It is now live and is viseble for customers!
+                    <v-btn vertical text dark
+                          @click="deal.snackbar = false">
+                      close
+                    </v-btn>
+                  </v-snackbar>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
 
-           </v-card-actions>
+            <v-btn fab text @click="deal.dialog2 = true">
+              <v-icon right>mdi-pencil</v-icon>
+            </v-btn>
+
+            <v-dialog v-model="deal.dialog2"
+                      max-width="290">
+              <v-card>
+                <v-layout row class="mx-auto">
+                  <v-card-title class="headline">Edit deal</v-card-title>
+                  <v-spacer></v-spacer>
+                  <v-btn icon @click="deal.dialog2 = false">
+                    <v-icon>mdi-close</v-icon>
+                  </v-btn>
+                </v-layout>
+                <v-form ref="form"
+                        v-model="valid"
+                        lazy-validation
+                        class="mx-5">
+
+                  <v-text-field v-model="title"
+                                label="Title"
+                                required
+                                color="#DFA937">
+                  </v-text-field>
+                  <v-textarea v-model="description"
+                              color="#DFA937">
+                    <template v-slot:label>
+                      <div>
+                        Description 
+                      </div>
+                    </template>
+                  </v-textarea>
+                  <v-checkbox v-model="allday"
+                              color="green">
+                    <template  v-slot:label>
+                      <div>
+                        All day deal
+                      </div>
+                    </template>
+                  </v-checkbox>
+                  <v-text-field v-model="start"
+                                label="Start-time"
+                                required
+                                color="#DFA937">
+                  </v-text-field>
+                  <v-text-field v-model="end"
+                                label="End-time"
+                                required
+                                color="#DFA937">
+                  </v-text-field>
+                  <!-- <v-text-field
+                    v-model="photo"
+                    label="Deal Photo"
+                    color="#DFA937"
+                    append-icon="mdi-camera-plus"
+                  ></v-text-field> -->
+                  <v-file-input label="Deal Photo"
+                                prepend-icon="mdi-camera">
+                  </v-file-input>
+                </v-form>
+
+                <v-card-actions class="d-flex justify-space-around pb-3">
+                  <v-btn
+                    width="50%" dark color="#DFA937" tile class="buttons" depressed @click="deal.dialog2 = false">
+                    save & post
+                  </v-btn>
+                  <v-btn
+                    width="40%" dark color="#DFA937" tile class="buttonst" depressed @click="deal.dialog2 = false">
+                    save
+                  </v-btn>
+                  <v-snackbar v-model="snackbar">
+                    You have successfully edited this deal. Toggle the switch to make the deal live and visable for customers!
+                    <v-btn color="success"
+                          vertical
+                          text
+                          dark
+                          @click="deal.snackbar = false">
+                      close
+                    </v-btn>
+                  </v-snackbar>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+
+            <v-btn fab text @click="deal.dialog = true">
+              <v-icon right>mdi-delete</v-icon>
+            </v-btn>
+
+            <v-dialog v-model="deal.dialog"
+                      max-width="290">
+              <v-card>
+                <v-layout row class="mx-auto">
+                  <v-card-title class="headline">Delete Deal</v-card-title>
+                  <v-spacer></v-spacer>
+                  <v-btn icon @click="deal.dialog = false">
+                    <v-icon>mdi-close</v-icon>
+                  </v-btn>
+                </v-layout>
+
+                <v-card-text>
+                  Are you sure you want to cancel this deal? This action is permanent.
+                </v-card-text>
+
+                <v-card-actions class="d-flex justify-center pb-3">
+                  <v-btn width="80%" dark color="#DFA937" tile class="buttons" depressed @click="remove(index)">
+                    yes, i am sure
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+
+            <v-snackbar v-model="snackbar">
+              You have successfully activated this deal. It is now live and is viseble for customers!
+              <v-btn color="success"
+                      vertical
+                      text
+                      dark
+                      @click="snackbar = false">
+                close
+              </v-btn>
+            </v-snackbar>
+          </v-row>	
+        </v-layout>
       </v-card>
-    </v-dialog>
-    <v-snackbar
-			v-model="snackbar">
-			You have successfully activated this deal. It is now live and is viseble for customers!
-			<v-btn
-			color="success"
-			vertical
-			text
-			dark
-			@click="snackbar = false">close</v-btn>
-			</v-snackbar>
-        </v-row>	
-     </v-layout>
-  </v-card>
-</v-layout>
-
+    </v-layout>
 	</div>
 </template>
 
