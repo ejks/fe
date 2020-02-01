4ca8acee13d2725316ac6e2a3ef331ecd1c07e82
KittyChen <kimikamis@gmail.com>
2020-02-01 22:52:59 +0800

    modify format

--- src/views/Vendor.vue
+++ src/views/Vendor.vue
@@ -9,106 +9,88 @@
         alt="John"
       >
 			</v-avatar>
-			<v-btn depressed dark class="button mb-6" width="80%" @click="dialog = true">add new deal
+			<v-btn depressed dark class="button mb-6" width="80%" @click="dialog = true">
+				add new deal
 			</v-btn>
-			<p class="headline font-weight-medium mb-0">BoomBoom</p>
+			<p class="headline font-weight-medium mb-0">{{ vendor.name }}</p>
 			<p class="subtitle-1">Café</p>
 			<v-layout row class="justify-end mx-5">
 				<v-btn text class="mt-n12 pt-0">
 					<v-icon right>mdi-pencil</v-icon>
 				</v-btn>
-				<v-dialog
-      v-model="dialog"
-      max-width="290">
-      <!-- <v-card> -->
-			
-		<v-card>
-			<v-layout row class="mx-auto">
-        <v-card-title class="headline">Add new Deal</v-card-title>
-		<v-spacer></v-spacer>
-        <v-btn icon @click="dialog = false">
-            <v-icon>mdi-close</v-icon>
-          </v-btn>
-        </v-layout>
-				<v-form
-    ref="form"
-    v-model="valid"
-    lazy-validation
-    class="mx-5"
-  >
+				<v-dialog v-model="dialog"
+									max-width="290">
+					<v-card>
+						<v-layout row class="mx-auto">
+							<v-card-title class="headline">Add new Deal</v-card-title>
+							<v-spacer></v-spacer>
+							<v-btn icon @click="dialog = false">
+								<v-icon>mdi-close</v-icon>
+							</v-btn>
+						</v-layout>
 
-    <v-text-field
-      v-model="title"
-      label="Title"
-      required
-      color="#DFA937"
-    ></v-text-field>
-    <v-textarea
-        v-model="description"
-        color="#DFA937"
-        >
-        <template v-slot:label>
-            <div>
-              Description 
-            </div>
-        </template>
-    </v-textarea>
-	<v-checkbox
-	v-model="allday"
-	color="green"
-	>
-	<template  v-slot:label>
-		<div>
-			All day deal
-		</div>
-	</template>
-	</v-checkbox>
-	<v-text-field
-      v-model="start"
-      label="Start-time"
-      required
-      color="#DFA937"
-    ></v-text-field>
-    <v-text-field
-      v-model="end"
-      label="End-time"
-      required
-      color="#DFA937"
-    ></v-text-field>
-    <v-file-input
-		label="Deal Photo"
-		prepend-icon="mdi-camera"
-	></v-file-input>
-	
-		
-  </v-form>
-<!-- </v-layout> -->
-<!-- </v-card> -->
+						<v-form ref="form"
+										v-model="valid"
+										lazy-validation
+										class="mx-5">
 
-        <v-card-actions class="d-flex justify-space-around pb-3">
+							<v-text-field v-model="title"
+														label="Title"
+														required
+														color="#DFA937">
+							</v-text-field>
+							<v-textarea v-model="description"
+													color="#DFA937">
+								<template v-slot:label>
+									<div>
+										Description 
+									</div>
+								</template>
+							</v-textarea>
+							<v-checkbox v-model="allday"
+													color="green">
+								<template v-slot:label>
+									<div>
+										All day deal
+									</div>
+								</template>
+							</v-checkbox>
+							<v-text-field v-model="start"
+														label="Start-time"
+														required
+														color="#DFA937">
+							</v-text-field>
+							<v-text-field v-model="end"
+														label="End-time"
+														required
+														color="#DFA937">
+							</v-text-field>
+							<v-file-input label="Deal Photo"
+														prepend-icon="mdi-camera">
+							</v-file-input>
+						</v-form>
 
-          <v-btn
-            width="50%" dark color="#DFA937" tile class="buttons" depressed @click="dialog = false">
-            save & post
-          </v-btn>
-          <v-btn
-            width="40%" dark color="#DFA937" tile class="buttonst" depressed @click="dialog = false">
-            save
-          </v-btn>
-           </v-card-actions>
-      </v-card>
-    </v-dialog>
-		</v-layout>
+						<v-card-actions class="d-flex justify-space-around pb-3">
+							<v-btn width="50%" dark color="#DFA937" tile class="buttons" depressed @click="dialog = false">
+								save & post
+							</v-btn>
+							<v-btn width="40%" dark color="#DFA937" tile class="buttonst" depressed @click="dialog = false">
+								save
+							</v-btn>
+						</v-card-actions>
+					</v-card>
+    		</v-dialog>
+			</v-layout>
 		</v-col>
 		<v-layout row class="mx-9">
 			<p class="title font-weight-bold">Description</p>
 			<p class="body1 font-weight-regular">From an upscale cocktail lounge to a burger and fries dive, there are many different personalities your new restaurant or bar can assume. Defining your mission statement is an important way that you can attract the right customers and reinforce your goals, which can set you up for long-term success.</p>
 			<p class="title font-weight-bold">Saved Deals</p>
 			<VendorCard />
-			
 		</v-layout>
 	</div>
 </template>
+
 <script>
 import VendorCard from '../components/VendorCard.vue'
 export default {
@@ -120,9 +102,9 @@ export default {
 			dialog2: false,
 		}
 	}
-
 }
 </script>
+
 <style>
 	.vendor {
 		background-color: #FFF;
